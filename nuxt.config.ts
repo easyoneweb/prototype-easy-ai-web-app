5dcaaa355440a0fc2cf13bb42c1dbd986b3339b0
Kirill Makeev <44197850+ikirja@users.noreply.github.com>
2024-12-14 00:12:53 +0300

    app name and version, update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
   runtimeConfig: {
     API_SERVER: process.env.API_SERVER || 'http://localhost:4000',
   },
-
   css: ['~/assets/css/main.css'],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
   compatibilityDate: '2024-12-13',
 });
